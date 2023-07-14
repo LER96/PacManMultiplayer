ed49aafff44f0d08ccb320014451739c8e6c1da4
TalTsadikov <taltsadikov@gmail.com>
2023-07-14 19:49:29 +0300

    team select and assigning pacman

--- MultiPlayerFinal/Assets/Scripts/TeamScripts/TeamManager.cs
+++ MultiPlayerFinal/Assets/Scripts/TeamScripts/TeamManager.cs
@@ -2,12 +2,16 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class TeamManager : MonoBehaviour
+public class TeamManager : MonoBehaviourPunCallbacks
 {
     List<Player> _teamPm = new List<Player>();
     List<Player> _teamMsPm = new List<Player>();
+
+    public string JOIN_TEAM_PM { get; private set; } = nameof(JoinTeamPM);
+    public string JOIN_TEAM_MSPM { get; private set; } = nameof(JoinTeamMsPM);
 
     [PunRPC]
     public void JoinTeamPM(Player _player)
--- MultiPlayerFinal/Assets/Scripts/TeamScripts/TeamSelectionManager.cs
+++ MultiPlayerFinal/Assets/Scripts/TeamScripts/TeamSelectionManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Linq;
 
 public class TeamSelectionManager : MonoBehaviourPunCallbacks
 {
@@ -14,15 +15,41 @@ public class TeamSelectionManager : MonoBehaviourPunCallbacks
     [SerializeField] TextMeshProUGUI _teamPmMembersText;
     [SerializeField] TextMeshProUGUI _teamMsPmMembersText;
 
+    private void Start()
+    {
+        AssignPacMan();
+    }
+
     public void JoinTeamPM()
     {
         _teamPmMembersText.text = PhotonNetwork.LocalPlayer.NickName;
-        photonView.RPC("JoinTeamPM", PhotonNetwork.LocalPlayer);
+        photonView.RPC(_teamManager.JOIN_TEAM_PM, RpcTarget.All ,PhotonNetwork.LocalPlayer);
     }
   
     public void JoinTeamMsPM()
     {
         _teamMsPmMembersText.text = PhotonNetwork.LocalPlayer.NickName;
-        photonView.RPC("JoinTeamMsPM", PhotonNetwork.LocalPlayer);
+        photonView.RPC(_teamManager.JOIN_TEAM_MSPM, RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+
+    public void AssignPacMan()
+    {
+        List<Player> playerList = PhotonNetwork.CurrentRoom.Players.Values.ToList();
+
+        if (playerList.Count >= 2)
+        {
+            for (int i = 0; i < playerList.Count; i++)
+            {
+                int randomIndex = Random.Range(i, playerList.Count);
+                Player temp = playerList[randomIndex];
+                playerList[randomIndex] = playerList[i];
+                playerList[i] = temp;
+            }
+
+            Debug.Log($"{playerList[0]} is Pacman");
+            Debug.Log($"{playerList[1]} is Miss Pacman");
+        }
+        else
+            Debug.Log("Not enough players in room");
     }
 }
