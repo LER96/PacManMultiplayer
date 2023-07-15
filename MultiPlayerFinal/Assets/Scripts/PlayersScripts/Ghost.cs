3f2b286ac535bdf360bdb649e535787e4d66cdb7
Liron Rotman <93140643+LER96@users.noreply.github.com>
2023-07-15 19:42:08 +0300

    spawnManager

--- MultiPlayerFinal/Assets/Scripts/PlayersScripts/Ghost.cs
+++ MultiPlayerFinal/Assets/Scripts/PlayersScripts/Ghost.cs
@@ -6,21 +6,24 @@ public class Ghost : Movement
 {
     public override void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (canMove)
         {
-            this.SetDirection(Vector2.up);
-        }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            this.SetDirection(Vector2.down);
-        }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            this.SetDirection(Vector2.right);
-        }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            this.SetDirection(Vector2.left);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                this.SetDirection(Vector2.up);
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                this.SetDirection(Vector2.down);
+            }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                this.SetDirection(Vector2.right);
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                this.SetDirection(Vector2.left);
+            }
         }
         base.Update();
     }
